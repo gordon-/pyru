f605f5454ddfe027a22118e5aa9764a8863f9674
Damien Nicolas <git@gordon.re>
2015-10-15 14:42:52 +0200

    enhanced admin listings

--- contacts/admin.py
+++ contacts/admin.py
@@ -8,39 +8,66 @@ from .models import (
 
 @admin.register(Properties)
 class PropertiesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'group', 'order', 'display_on_list')
+    list_filter = ('group', 'display_on_list')
+    search_fields = ('name', )
 
 
 @admin.register(Alert)
 class AlertAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'group', 'contact', 'priority', 'date',
+                    'done')
+    list_filter = ('user', 'priority', 'done', 'date')
+    search_fields = ('contact__firstname', 'contact__lastname',
+                     'user__username')
+
+    def group(self, object):
+        return object.contact.group
+    group.short_description = 'Groupe'
 
 
 @admin.register(ContactType)
 class ContactTypeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'group')
+    list_filter = ('group', )
+    search_fields = ('name', )
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'group', 'type')
+    list_filter = ('group', 'type')
+    search_fields = ('name', )
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('firstname', 'lastname', 'company', 'group', 'type')
+    list_filter = ('group', 'type')
+    search_fields = ('firstname', 'lastname', 'company')
 
 
 @admin.register(MeetingType)
 class MeetingTypeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'group')
+    list_filter = ('group', )
+    search_fields = ('name', )
 
 
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'group', 'contact', 'type', 'date')
+    list_filter = ('author', 'type', 'date')
+    search_fields = ('contact__firstname', 'contact__lastname',
+                     'author__username')
+
+    def group(self, object):
+        return object.contact.group
+    group.short_description = 'Groupe'
 
 
 @admin.register(SavedSearch)
 class SavedSearchAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'group', 'type', 'results_count')
+    list_filter = ('group', 'type')
+    search_fields = ('name', )
