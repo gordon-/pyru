7f7411a66f11b06b6675ec419d98cbd5f07574c4
Damien Nicolas <damien@gordon.re>
2015-11-26 11:00:53 +0100

    fix: Create a group and assign current user to it if user has no groups (fixes #15)

--- contacts/middleware.py
+++ contacts/middleware.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Group
+
 from .models import DefaultGroup
 
 
@@ -8,6 +10,10 @@ class DefaultGroupMiddleware:
             if request.user.is_authenticated():
                 request.user.default_group
         except DefaultGroup.DoesNotExist:
+            if request.user.groups.count() == 0:
+                group = Group(name=request.user.username)
+                group.save()
+                request.user.groups.add(group)
             DefaultGroup.objects.create(
                 user=request.user,
                 group=request.user.groups.first())
