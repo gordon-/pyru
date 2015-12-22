faca19bd2984b6e9cc2cd649b3024ab98f70d7fa
Damien Nicolas <damien@gordon.re>
2015-12-22 15:56:30 +0100

    Add testing dependencies

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from . import models
+
+
+def test_apply_mapping():
+    row = {'key': 'val'}
+    mapping = {'new_key': 'key'}
+    res = models.apply_mapping(row, mapping)
+    assert isinstance(res, dict)
+    assert 'new_key' in res
+    assert res['new_key'] == 'val'
+    assert 'key' not in res
+
+    row = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'}
+    mapping = {'k1_': 'k1', 'k2_': 'k2'}
+    res = models.apply_mapping(row, mapping)
+    assert 'k1_' in res
+    assert 'k3' in res
+    assert 'k2' not in res
+
+    # keep the same row
+    mapping = {'k4_': 'k4'}
+    exception_raised = False
+    try:
+        models.apply_mapping(row, mapping)
+    except models.DataImportError:
+        exception_raised = True
+    assert exception_raised
