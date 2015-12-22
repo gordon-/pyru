50b4234c9b1e8afdca5151beb8c4d2d2bb8c5032
Damien Nicolas <damien@gordon.re>
2015-12-22 16:16:34 +0100

    Remove JS debug statements

--- contacts/static/js/readmore.js
+++ contacts/static/js/readmore.js
@@ -3,8 +3,6 @@ $(function() {
     $('.readmore').each(function() {
         var el_height = $(this).height();
         if(el_height > max_height) {
-            console.log($(this).text(), el_height);
-            //debugger;
             var readmore_link = $('<div/>', {class: 'readmore-link text-center'});
             readmore_link.append($('<a>Lire plus</a>'));
             $(this).data('height', el_height + readmore_link.height());
@@ -17,7 +15,6 @@ $(function() {
 
     $('body').on('click', '.readmore-link', function(ev) {
         var el = $(this).parent();
-        debugger;
         el.animate({height: el.data('height')}, el.data('speed'));
         $(this).removeClass('readmore-link').addClass('readless-link');
         $('a', this).text('Lire moins');
